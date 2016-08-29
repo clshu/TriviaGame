e9eab7350a3ff4c5b9a5d63bc2a59accf1b61845
clshu <ccss0630@yahoo.com>
2016-08-28 21:14:28 -0500

    Create the frames for the layout design and add some jpg and data for testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,43 @@ $(document).ready(readyFn);
 
 function readyFn() {
 	console.log("Hi it's working");
+	console.log(gameData[1]);
 }
+
+// Game Data
+
+var gameData = [
+{
+	answer: "Marilyn Monroe",
+	question: "Norma Jeane Mortenson was better known under her stage name:",
+	choices: ["Natalie Wood", "Doris Day", "Lauren Bacall", "Marilyn Monroe"],
+	comment: "Marilyn Monroe, born June 1, 1926,  was an American actress and model. Famous for playing 'dumb blonde' characters, she became one of the most popular sex symbols of the 1950s, emblematic of the era's attitudes towards sexuality.",
+	img: "marilyn.jpg"
+},
+
+{
+	answer: "Peter Fonda and Dennis Hopper",
+	question: "What actors play the main characters in the American road movie Easy Rider?",
+	choices: ["Clint Eastwood and Burt Reynolds", "Warren Beatty and Alan Alda", "Peter Fonda and Dennis Hopper", "Dustin Hoffman and Paul Newman"],
+	comment: "",
+	img: "peterfonda.jpg"
+},
+
+{
+	answer: "Steve McQueen",
+	question: "Who plays the leading character in The Thomas Crown Affair from 1968?",
+	choices: ["Steve McQueen", " Clint Eastwood", "Albert Finney", "George Segal"],
+	comment: "The Thomas Crown Affair is a 1968 film directed and produced by Norman Jewison. Steve McQueen plays the leading character, Thomas Crown. Faye Dunaway plays the investigator, Vicki Anderson. The film was nominated for two Academy Awards, winning Best Original Song for Michel Legrand's Windmills of Your Mind",
+	img: "crownaffair.jpg"
+}
+/*
+var mv2 = {
+	answer: "",
+	question: "",
+	choices: ["", "", "", ""],
+	comment: "",
+	img: ""
+}
+*/
+];
+
